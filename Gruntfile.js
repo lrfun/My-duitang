df85340ada3d2ceb11e9576bf7711dced8ef2b54
lrfun <luorongfang126@126.com>
2015-10-15 13:47:26 +0800

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,43 +1,61 @@
 module.exports = function(grunt) {
 
-  var sassStyle = 'expanded';
+  var lessStyle = 'expanded';
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    sass: {
-      output : {
-        options: {
-          style: sassStyle
-        },
-        files: {
-          './style.css': './scss/style.scss'
-        }
+    less: {
+      development: {
+          files: [{
+              expand: true,
+              cwd: './less/',
+              src: ['**/*.less'],
+              dest: './css/',
+              ext: '.css'
+          }]
       }
+    },
+    imagemin: {
+      /* 压缩图片大小 */
+      dist: {
+          options: {
+              optimizationLevel: 3 //定义 PNG 图片优化水平
+          },
+          files: [
+                 {
+              expand: true,
+              cwd: 'images/',
+              src: ['**/*.{png,jpg,jpeg}'], // 优化 img 目录下所有 png/jpg/jpeg 图片
+              dest: 'images/' // 优化后的图片保存位置，覆盖旧图片，并且不作提示
+              }
+              ]
+          }
     },
     concat: {
       dist: {
-        src: ['./src/plugin.js', './src/plugin2.js'],
-        dest: './global.js',
+        src: ['./js/plugin.js', './js/plugin2.js'],
+        dest: './js/global.js',
       },
     },
     uglify: {
       compressjs: {
         files: {
-          './global.min.js': ['./global.js']
+          './js/global.min.js': ['./js/global.js']
         }
       }
     },
     jshint: {
-      all: ['./global.js']
+      all: ['./js/global.js']
     },
     watch: {
       scripts: {
-        files: ['./src/plugin.js','./src/plugin2.js'],
+        files: ['./js/plugin.js','./js/plugin2.js'],
         tasks: ['concat','jshint','uglify']
       },
-      sass: {
-        files: ['./scss/style.scss'],
-        tasks: ['sass']
+
+      less: {
+        files: ['./less/main.less'],
+        tasks: ['less']
       },
       livereload: {
           options: {
@@ -45,14 +63,14 @@ module.exports = function(grunt) {
           },
           files: [
               'index.html',
-              'style.css',
+              'css/main.css',
               'js/global.min.js'
           ]
       }
     },
     connect: {
       options: {
-          port: 9000,
+          port: 8080,
           open: true,
           livereload: 35729,
           // Change this to '0.0.0.0' to access the server from outside
@@ -60,24 +78,26 @@ module.exports = function(grunt) {
       },
       server: {
         options: {
-          port: 9001,
+          port: 8080,
           base: './'
         }
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-connect');
 
-  grunt.registerTask('outputcss',['sass']);
+  grunt.registerTask('outputcss',['less']);
+   grunt.registerTask('img', ['imagemin']);
   grunt.registerTask('concatjs',['concat']);
   grunt.registerTask('compressjs',['concat','jshint','uglify']);
-  grunt.registerTask('watchit',['sass','concat','jshint','uglify','connect','watch']);
+  grunt.registerTask('watchit',['less','concat','jshint','uglify','connect','watch']);
   grunt.registerTask('default');
 
 };
