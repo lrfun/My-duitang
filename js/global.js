9b969719d1e5b8746f72e7ec4015a94bbd463b26
lrfun <luorongfang126@126.com>
2015-10-16 14:02:46 +0800

    优化菜单隐藏显示功能

--- js/global.js
+++ js/global.js
@@ -1,18 +1,17 @@
 
 
-//首页头部鼠标经过显示导航菜单
+// 首页头部鼠标经过显示导航菜单
 $(document).ready(function() {
 	$('#nav').mouseover(function () {
-		$(this).find('#subNav').show();
-		$('#navBox').css({"height":"42px","border-bottom":"0","z-index":"1000"});
+		$(this).find('#subNav').show();		
+	}).mouseout(function(){
+		$(this).find('#subNav').hide();
 	})
 
 	$('#subNav').mouseover(function () {
-		$(this).show();
-		$('#navBox').css({"height":"42px","border-bottom":"0","z-index":"1000"});
+		$(this).show();		
 	}).mouseout(function(){
-		$(this).hide();
-		$('#navBox').css({"height":"26px","z-index":"1000","border-bottom":"1px solid #E0E0E0"});
+		$(this).hide();		
 	})
 
 	//首页头部鼠标经过显示关联账号
@@ -30,3 +29,25 @@ $(document).ready(function() {
 
 
 })
+
+// $(document).ready(function() {
+// 	show_Sub(nav,subNav);
+// 	showSub(relLogin,relNum_more);
+// });
+
+// function show_Sub(a,b){
+// 	$('#a').mouseover(function(){
+// 		$(this).find('#b').show();
+// 	}).mouseout(function(){
+// 		$(this).find('#b').hide();
+// 	});
+// 	$('#b').mouseover(function(){
+// 		$(this).show();
+// 	}).mouseout(function(){
+// 		$(this).hide();
+// 	})
+
+// }
+
+
+
