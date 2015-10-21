a5fa125cec67237440afe7ea28199b1c8218ca08
lrfun <luorongfang126@126.com>
2015-10-21 11:50:55 +0800

    点击圆点轮播

--- js/global.js
+++ js/global.js
@@ -29,16 +29,33 @@ $(document).ready(function() {
 	});
 
     //首页轮播
+
+     
      function DY_scroll(wraper, prev_page, next_page){
         var wrap = $(wraper);
         var prev = $(prev_page);
         var next = $(next_page);
-        var img = wraper.find('.bannerBar');
-        var w = img.find('.banner_img').outerWidth(true);      
+        var img = wrap.find('.bannerBar');
+        var w = img.find('.banner_img').outerWidth(true); 
+
+        //点击圆点轮播
+        $(".banner_num .banner_text li").each(function () {
+            $(this).click(function () {
+                var index=$(this).index();
+                var mal=index*w;
+                $(this).addClass("cur").siblings('li').removeClass('cur');
+                img.animate({'margin-left':-mal+"px"});
+
+            });
+        });   
+
+        //点击箭头轮播  
         next.click(function(){
             img.animate({'margin-left': -w}, function(){
-                img.find('.banner_img').eq(0).appendTo(img);
+                img.find('.banner_img').eq(0).appendTo(img);                               
                 img.css({'margin-left': 0});
+                var index=$(this).index();
+                alert(index); 
             });
         });
         prev.click(function(){
